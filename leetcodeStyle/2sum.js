ad7dcfabbc6379d3628af6bb9ed3f0c882dbe238
harryhmohney <harrison.mohney@gmail.com>
2024-03-28 20:50:16 -0700

    2sum

---
+++ leetcodeStyle/2sum.js
@@ -0,0 +1,10 @@
+function twoSum(nums, target) {
+  const map = {};
+  for (let i = 0; i < nums.length; i++) {
+      const complement = target - nums[i];
+      if (map.hasOwnProperty(complement)) {
+          return [map[complement], i];
+      }
+      map[nums[i]] = i;
+  }
+}
