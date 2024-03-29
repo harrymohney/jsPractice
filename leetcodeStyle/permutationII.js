ce5fcce80bae71c0a5264b3807ca76f898a90811
harryhmohney <harrison.mohney@gmail.com>
2024-03-29 15:43:08 -0700

    permutationII

---
+++ leetcodeStyle/permutationII.js
@@ -0,0 +1,29 @@
+function nextPermutation(nums) {
+  let i = nums.length - 2;
+  while (i >= 0 && nums[i] >= nums[i + 1]) {
+      i--;
+  }
+  if (i >= 0) {
+      let j = nums.length - 1;
+      while (j >= 0 && nums[j] <= nums[i]) {
+          j--;
+      }
+      swap(nums, i, j);
+  }
+  reverse(nums, i + 1);
+}
+
+function swap(nums, i, j) {
+  const temp = nums[i];
+  nums[i] = nums[j];
+  nums[j] = temp;
+}
+
+function reverse(nums, start) {
+  let left = start, right = nums.length - 1;
+  while (left < right) {
+      swap(nums, left, right);
+      left++;
+      right--;
+  }
+}
