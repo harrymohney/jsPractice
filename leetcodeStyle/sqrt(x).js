81cbd1e8dd55d287a8af2282f622a4a9073c90be
harryhmohney <harrison.mohney@gmail.com>
2024-03-29 15:30:39 -0700

    sqrt(x)

---
+++ leetcodeStyle/sqrt(x).js
@@ -0,0 +1,11 @@
+function mySqrt(x) {
+  let left = 0;
+  let right = x;
+  while (left <= right) {
+      const mid = Math.floor((left + right) / 2);
+      if (mid * mid === x) return mid;
+      if (mid * mid < x) left = mid + 1;
+      else right = mid - 1;
+  }
+  return right;
+}
