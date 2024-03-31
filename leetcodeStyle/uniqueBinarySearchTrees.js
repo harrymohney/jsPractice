585385dcfbe50e91d3815c8cd1557f9a5c4a6e5d
harryhmohney <harrison.mohney@gmail.com>
2024-03-30 23:09:47 -0700

    uniqueBinarySearchTrees

---
+++ leetcodeStyle/uniqueBinarySearchTrees.js
@@ -0,0 +1,10 @@
+function numTrees(n) {
+  const dp = Array(n + 1).fill(0);
+  dp[0] = dp[1] = 1;
+  for (let i = 2; i <= n; i++) {
+      for (let j = 1; j <= i; j++) {
+          dp[i] += dp[j - 1] * dp[i - j];
+      }
+  }
+  return dp[n];
+}
