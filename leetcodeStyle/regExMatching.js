6b2137ed17d3a674adb648828d9970ee64e355ef
harryhmohney <harrison.mohney@gmail.com>
2024-04-01 18:50:19 -0700

    regExMatching

---
+++ leetcodeStyle/regExMatching.js
@@ -0,0 +1,22 @@
+function isMatch(s, p) {
+  const dp = Array(s.length + 1).fill(false).map(() => Array(p.length + 1).fill(false));
+  dp[0][0] = true;
+  for (let i = 1; i <= p.length; i++) {
+      if (p[i - 1] === '*') {
+          dp[0][i] = dp[0][i - 2];
+      }
+  }
+  for (let i = 1; i <= s.length; i++) {
+      for (let j = 1; j <= p.length; j++) {
+          if (p[j - 1] === '.' || p[j - 1] === s[i - 1]) {
+              dp[i][j] = dp[i - 1][j - 1];
+          } else if (p[j - 1] === '*') {
+              dp[i][j] = dp[i][j - 2];
+              if (p[j - 2] === '.' || p[j - 2] === s[i - 1]) {
+                  dp[i][j] = dp[i][j] || dp[i - 1][j];
+              }
+          }
+      }
+  }
+  return dp[s.length][p.length];
+}
