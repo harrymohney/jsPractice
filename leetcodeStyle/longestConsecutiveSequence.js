4b381341c8f6b4be9c2b47734cf356aae553ed4b
harryhmohney <harrison.mohney@gmail.com>
2024-04-01 18:19:07 -0700

    longestConsecutiveSequence

---
+++ leetcodeStyle/longestConsecutiveSequence.js
@@ -0,0 +1,16 @@
+function longestConsecutive(nums) {
+  const numSet = new Set(nums);
+  let longestStreak = 0;
+  for (let num of numSet) {
+      if (!numSet.has(num - 1)) {
+          let currentNum = num;
+          let currentStreak = 1;
+          while (numSet.has(currentNum + 1)) {
+              currentNum++;
+              currentStreak++;
+          }
+          longestStreak = Math.max(longestStreak, currentStreak);
+      }
+  }
+  return longestStreak;
+}
