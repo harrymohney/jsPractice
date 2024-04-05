2ae612a06c245c2bee975980c9963474bdbb568e
harryhmohney <harrison.mohney@gmail.com>
2024-04-05 12:53:16 -0700

    intersectionOfTwoLinkedLists

---
+++ leetcodeStyle/intersectionOfTwoLinkedLists.js
@@ -0,0 +1,9 @@
+function getIntersectionNode(headA, headB) {
+  let p1 = headA;
+  let p2 = headB;
+  while (p1 !== p2) {
+      p1 = p1 ? p1.next : headB;
+      p2 = p2 ? p2.next : headA;
+  }
+  return p1;
+}
