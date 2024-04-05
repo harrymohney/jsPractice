8ffd52c010be8ce4ef56c474e5321d73338a77ed
harryhmohney <harrison.mohney@gmail.com>
2024-04-04 21:07:38 -0700

    pathSum

---
+++ leetcodeStyle/pathSum.js
@@ -0,0 +1,5 @@
+function hasPathSum(root, targetSum) {
+  if (root === null) return false;
+  if (root.left === null && root.right === null && root.val === targetSum) return true;
+  return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+}
