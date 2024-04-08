6a2d8c560a010ff3fb902c123fadb5ef137dfb9d
harryhmohney <harrison.mohney@gmail.com>
2024-04-08 00:09:43 -0700

    decodeWays

---
+++ leetcodeStyle/decodeWays.js
@@ -0,0 +1,17 @@
+function numDecodings(s) {
+  if (!s || s[0] === '0') return 0;
+  const dp = new Array(s.length + 1).fill(0);
+  dp[0] = 1;
+  dp[1] = 1;
+  for (let i = 2; i <= s.length; i++) {
+      const oneDigit = parseInt(s.substring(i - 1, i));
+      const twoDigits = parseInt(s.substring(i - 2, i));
+      if (oneDigit >= 1 && oneDigit <= 9) {
+          dp[i] += dp[i - 1];
+      }
+      if (twoDigits >= 10 && twoDigits <= 26) {
+          dp[i] += dp[i - 2];
+      }
+  }
+  return dp[s.length];
+}
