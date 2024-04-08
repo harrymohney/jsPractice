16bdbd03a509edf0d8dd72e49524ba6f2f94c9f0
harryhmohney <harrison.mohney@gmail.com>
2024-04-08 00:08:36 -0700

    findDuplicateNumber

---
+++ leetcodeStyle/FindDuplicateNumber.js
@@ -0,0 +1,16 @@
+function findDuplicate(nums) {
+  let slow = nums[0];
+  let fast = nums[0];
+  do {
+      slow = nums[slow];
+      fast = nums[nums[fast]];
+  } while (slow !== fast);
+  
+  let ptr1 = nums[0];
+  let ptr2 = slow;
+  while (ptr1 !== ptr2) {
+      ptr1 = nums[ptr1];
+      ptr2 = nums[ptr2];
+  }
+  return ptr1;
+}
