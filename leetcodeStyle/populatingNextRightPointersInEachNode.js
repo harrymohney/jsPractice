3953bb516600d1080bbe2757874418f08a6d46c1
harryhmohney <harrison.mohney@gmail.com>
2024-04-09 23:55:54 -0700

    populatingNextRightPointerInEachNode

---
+++ leetcodeStyle/populatingNextRightPointersInEachNode.js
@@ -0,0 +1,16 @@
+function connect(root) {
+  if (!root) return root;
+  let leftmost = root;
+  while (leftmost.left) {
+      let head = leftmost;
+      while (head) {
+          head.left.next = head.right;
+          if (head.next) {
+              head.right.next = head.next.left;
+          }
+          head = head.next;
+      }
+      leftmost = leftmost.left;
+  }
+  return root;
+}
