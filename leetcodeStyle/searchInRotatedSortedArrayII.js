1850a9d93c7c27bcc6a0fc894a91248d1ca34c4a
harryhmohney <harrison.mohney@gmail.com>
2024-04-09 23:42:30 -0700

    searchInRotatedSortedArrayII

---
+++ leetcodeStyle/searchInRotatedSortedArrayII.js
@@ -0,0 +1,24 @@
+function search(nums, target) {
+  let left = 0, right = nums.length - 1;
+  while (left <= right) {
+      const mid = Math.floor((left + right) / 2);
+      if (nums[mid] === target) return true;
+      if (nums[left] === nums[mid] && nums[mid] === nums[right]) {
+          left++;
+          right--;
+      } else if (nums[left] <= nums[mid]) {
+          if (target >= nums[left] && target < nums[mid]) {
+              right = mid - 1;
+          } else {
+              left = mid + 1;
+          }
+      } else {
+          if (target > nums[mid] && target <= nums[right]) {
+              left = mid + 1;
+          } else {
+              right = mid - 1;
+          }
+      }
+  }
+  return false;
+}
