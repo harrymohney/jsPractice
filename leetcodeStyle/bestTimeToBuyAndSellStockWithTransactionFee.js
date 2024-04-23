e24828582566b9e92b92d81de89f0681ae6bc95b
harryhmohney <harrison.mohney@gmail.com>
2024-04-23 11:22:07 -0700

    bestTimeToBuyAndSellStockWithTransactionFee

---
+++ leetcodeStyle/bestTimeToBuyAndSellStockWithTransactionFee.js
@@ -0,0 +1,9 @@
+function maxProfit(prices, fee) {
+  let cash = 0;
+  let hold = -prices[0];
+  for (let i = 1; i < prices.length; i++) {
+      cash = Math.max(cash, hold + prices[i] - fee);
+      hold = Math.max(hold, cash - prices[i]);
+  }
+  return cash;
+}
