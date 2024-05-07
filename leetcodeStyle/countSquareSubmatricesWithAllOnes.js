0f8ccf329246ba995623c92172638ebfa70f40d1
harryhmohney <harrison.mohney@gmail.com>
2024-05-06 22:09:29 -0700

    countSquareSubmatricesWithAllOnes

---
+++ leetcodeStyle/countSquareSubmatricesWithAllOnes.js
@@ -0,0 +1,16 @@
+function countSquares(matrix) {
+  const m = matrix.length;
+  const n = matrix[0].length;
+  let count = 0;
+
+  for (let i = 0; i < m; i++) {
+      for (let j = 0; j < n; j++) {
+          if (matrix[i][j] === 1 && i > 0 && j > 0) {
+              matrix[i][j] += Math.min(matrix[i - 1][j], matrix[i][j - 1], matrix[i - 1][j - 1]);
+          }
+          count += matrix[i][j];
+      }
+  }
+
+  return count;
+}
