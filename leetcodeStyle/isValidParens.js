0d5ce988bb2a760087d78c3b48970df8b50c72ec
harryhmohney <harrison.mohney@gmail.com>
2024-05-22 00:01:40 -0700

    isValidParens

---
+++ leetcodeStyle/isValidParens.js
@@ -0,0 +1,20 @@
+function isValidParentheses(s) {
+  const stack = [];
+  const pairs = {
+      '(': ')',
+      '[': ']',
+      '{': '}'
+  };
+  for (let char of s) {
+      if (pairs[char]) {
+          stack.push(char);
+      } else {
+          if (stack.length === 0 || pairs[stack.pop()] !== char) {
+              return false;
+          }
+      }
+  }
+  return stack.length === 0;
+}
+// Test
+console.log(isValidParentheses("{[]}")); // Output: true
