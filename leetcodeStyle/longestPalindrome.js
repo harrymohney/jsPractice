fbe5dc773f4622c471f8f439d561f88a0e1f15ef
harryhmohney <harrison.mohney@gmail.com>
2024-05-22 00:04:49 -0700

    longestPalindrome

---
+++ leetcodeStyle/longestPalindrome.js
@@ -0,0 +1,22 @@
+function longestPalindrome(s) {
+  let longest = '';
+  for (let i = 0; i < s.length; i++) {
+      const oddPalindrome = expandAroundCenter(s, i, i);
+      const evenPalindrome = expandAroundCenter(s, i, i + 1);
+      const currentLongest = oddPalindrome.length > evenPalindrome.length ? oddPalindrome : evenPalindrome;
+      if (currentLongest.length > longest.length) {
+          longest = currentLongest;
+      }
+  }
+  return longest;
+}
+
+function expandAroundCenter(s, left, right) {
+  while (left >= 0 && right < s.length && s[left] === s[right]) {
+      left--;
+      right++;
+  }
+  return s.substring(left + 1, right);
+}
+// Test
+console.log(longestPalindrome("babad")); // Output: "bab" or "aba"
