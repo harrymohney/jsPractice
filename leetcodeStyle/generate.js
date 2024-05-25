e20c2aa6fdb5672892b69479d63ddbb162a1ad04
harryhmohney <harrison.mohney@gmail.com>
2024-05-24 22:36:42 -0700

    generate

---
+++ leetcodeStyle/generate.js
@@ -0,0 +1,15 @@
+function generate(numRows) {
+  if (numRows === 0) return [];
+  const triangle = [[1]];
+  for (let i = 1; i < numRows; i++) {
+      const row = [1];
+      for (let j = 1; j < i; j++) {
+          row.push(triangle[i - 1][j - 1] + triangle[i - 1][j]);
+      }
+      row.push(1);
+      triangle.push(row);
+  }
+  return triangle;
+}
+// Test
+console.log(generate(5)); // Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
