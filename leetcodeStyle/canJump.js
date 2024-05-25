95af55a5856119d504d2c00dfafd539607bbec38
harryhmohney <harrison.mohney@gmail.com>
2024-05-24 22:21:03 -0700

    canJump

---
+++ leetcodeStyle/canJump.js
@@ -0,0 +1,11 @@
+function canJump(nums) {
+  let maxReach = 0;
+  for (let i = 0; i < nums.length; i++) {
+      if (i > maxReach) return false;
+      maxReach = Math.max(maxReach, i + nums[i]);
+      if (maxReach >= nums.length - 1) return true;
+  }
+  return false;
+}
+// Test
+console.log(canJump([2, 3, 1, 1, 4])); // Output: true
