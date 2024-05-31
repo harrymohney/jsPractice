07dc9f3a7cad650499b380c411775e83b23ef93f
harryhmohney <harrison.mohney@gmail.com>
2024-05-30 22:37:07 -0700

    romanToIntII

---
+++ leetcodeStyle/romanToIntII.js
@@ -0,0 +1,22 @@
+function romanToInt(s) {
+  const romanMap = {
+      'I': 1,
+      'V': 5,
+      'X': 10,
+      'L': 50,
+      'C': 100,
+      'D': 500,
+      'M': 1000
+  };
+  let result = 0;
+  for (let i = 0; i < s.length; i++) {
+      if (i < s.length - 1 && romanMap[s[i]] < romanMap[s[i + 1]]) {
+          result -= romanMap[s[i]];
+      } else {
+          result += romanMap[s[i]];
+      }
+  }
+  return result;
+}
+// Test
+console.log(romanToInt("MCMXCIV")); // Output: 1994
