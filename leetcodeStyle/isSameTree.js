19c5bf54ccbc83d9e307be0a061b0eb185da1364
harryhmohney <harrison.mohney@gmail.com>
2024-06-02 22:29:01 -0700

    isSameTree

---
+++ leetcodeStyle/isSameTree.js
@@ -0,0 +1,9 @@
+function isSameTree(p, q) {
+  if (!p && !q) return true;
+  if (!p || !q || p.val !== q.val) return false;
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+}
+// Test
+const tree1 = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+const tree2 = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+console.log(isSameTree(tree1, tree2)); // Output: true
