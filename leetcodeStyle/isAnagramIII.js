93ae227a755dfc30c74ea9961ef4b8e51c5b2d3f
harryhmohney <harrison.mohney@gmail.com>
2024-07-12 11:32:58 -0700

    isAnagramIII

---
+++ leetcodeStyle/isAnagramIII.js
@@ -0,0 +1,12 @@
+function isAnagram(s, t) {
+  if (s.length !== t.length) return false;
+  const count = {};
+  for (const char of s) count[char] = (count[char] || 0) + 1;
+  for (const char of t) {
+      if (!count[char]) return false;
+      count[char]--;
+  }
+  return true;
+}
+// Test
+console.log(isAnagram("anagram", "nagaram")); // Output: true
